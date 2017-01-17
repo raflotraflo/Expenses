a2e3944bb3d35edc45610713dfa79b03f5c972f2
Rafał Chodzidło <rchodzidlo@aiut.com.pl>
2017-01-17 13:49:41 +0100

    ConsoleApp was added for quickly tests

---
+++ Expenses.QuicklyTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using Expenses.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Expenses.Domain.Interfaces;
+
+namespace Expenses.QuicklyTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            try
+            {
+                RepositoryContext db = new RepositoryContext("RepositoryContext");
+
+                IUserRepository userRepository = new UserRepository(db);
+
+                var allUsers = userRepository.GetAllUsers().ToList();
+            }
+            catch (Exception ex)
+            {
+                ;
+            }
+
+
+            Console.WriteLine("Hellow world");
+            Console.ReadKey();
+        }
+    }
+}
