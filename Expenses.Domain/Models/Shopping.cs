d5af84a42de38778236e1f8ffd6479b401551b49
Rafał Chodzidło <rchodzidlo@aiut.com.pl>
2017-01-17 12:30:18 +0100

    Base models was added

---
+++ Expenses.Domain/Models/Shopping.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses.Domain.Models
+{
+    class Shopping
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public  Category Category { get; set; }
+        public DateTime Timestamp { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
